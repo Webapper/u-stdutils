9f52e53b952566fd3dfee6ba907a4f0706fcfb3b
assarte <be.creative@webapper.hu>
2016-05-21 01:23:15 +0200

    + Strings class + Strings::hexdump() method

---
+++ Webapper/U/Utils/Strings.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Copyright (c) 2016. by Csaba Dobai (aka Assarte), all rights reserved due to European Laws of Intellectual Properties OR licence attached
+ */
+
+/**
+ * Created by PhpStorm.
+ * User: assarte
+ * Date: 2016.05.21.
+ * Time: 1:18
+ */
+
+namespace Webapper\U\Utils;
+
+class Strings
+{
+    /**
+     * Returns the string in escaped hexa-decimal form, eg. "Hello\r\n" will be '\0x48\0x65\0x6c\0x6c\0x6f\0x0d\0x0a'
+     * @param string $blob
+     * @return string
+     */
+    public static function hexdump($blob) {
+        $result = '';
+        for ($i = 0; $i < strlen($blob); $i++) {
+            $result .= str_pad(dechex(ord($blob{$i})), 5, '\\0x0', STR_PAD_LEFT);
+        }
+        return $result;
+    }
+}
